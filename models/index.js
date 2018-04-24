0c80e39c3e2b63d79385a64267f62842a1459f2f
trevans24 <trevans24@gmail.com>
2018-04-24 12:07:20 -0600

    typo

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 'use strict'
 
 const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGODV_URI || process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost/bond-cars');
+mongoose.connect(process.env.MONGODB_URI || process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost/bond-cars');
 
 module.exports.Car = require('./cars.js');
